da32905ac0812960eb6d8f7025db844a69eee67d
Didan54 <msadidan05042003@gmail.com>
2025-09-04 01:31:03 +0800

    force headless opencv on railway

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip uninstall -y opencv-python opencv-contrib-python || true
+pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless
 pip install opencv-python-headless==4.8.0.74
 python esp32cam.py
